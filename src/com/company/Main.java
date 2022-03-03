f398f77d76495ea86c8fbe6b244716fca4a24636
Beknaza <bajnazarovbeknazar@gmail.com>
2022-03-03 18:27:24 +0600

    gitOpen

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ageTemperature(100,100));
+
+    }
+
+    public static String ageTemperature(int age, int temperature) {
+        String convert_1 = "Можно идти гулять";
+        String convert_3 = "Оставайтесь дома";
+        if (age > 20 && age < 45 && temperature > -20 && temperature < 30) {
+            return convert_1;
+        } else if (age < 20 && temperature > 0 && temperature < 28) {
+            return convert_1;
+        } else if (age > 45 && temperature > -10 && temperature < 25) {
+            return convert_1;
+        } else if (age > 45 && temperature < -10 && temperature > 25) {
+            return convert_3;
+        }
+        return convert_3;
+    }
+
+}
